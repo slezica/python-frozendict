cd5b529cbcce4e86f3c5ddc99b5f1d900fdebc0c
Chih-Hsuan Yen <yan12125@gmail.com>
2018-11-22 16:57:19 +0800

    Fix warnings from collections
    Specifically, the following DeprecationWarning due to changes in Python
    3.7 [1]
    
    /home/yen/Projects/python-frozendict/frozendict/__init__.py:16: DeprecationWarning: Using or importing the ABCs from 'collections' instead of from 'collections.abc' is deprecated, and in 3.8 it will stop working
      class frozendict(collections.Mapping):
    
    [1] https://github.com/python/cpython/pull/5460

--- frozendict/__init__.py
+++ frozendict/__init__.py
@@ -9,11 +9,16 @@ try:
 except ImportError:  # python < 2.7
     OrderedDict = NotImplemented
 
+try:
+    from collections.abc import Mapping
+except ImportError:  # python < 3.3
+    from collections import Mapping
+
 
 iteritems = getattr(dict, 'iteritems', dict.items) # py2-3 compatibility
 
 
-class frozendict(collections.Mapping):
+class frozendict(Mapping):
     """
     An immutable wrapper around dictionaries that implements the complete :py:class:`collections.Mapping`
     interface. It can be used as a drop-in replacement for dictionaries where immutability is desired.
