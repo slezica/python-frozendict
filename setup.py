f89d45fbd0633e6bf68871d24052bbbe316cecf0
Mikalai Saskavets <shurph@gmail.com>
2019-04-18 23:54:21 +0300

    Add to classifiers supported python versions
    It will help those people who will do some version compatability
    checking with officially recomended tool **caniusepython3** which
    looks exactly into classifiers to detect that an examined lib supports
    Python 3.

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@ setup(
     author       = 'Santiago Lezica',
     author_email = 'slezica89@gmail.com',
 
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
     packages = ['frozendict'],
     license  = 'MIT License',
 
