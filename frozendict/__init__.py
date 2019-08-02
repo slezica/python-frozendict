b4b96da99b47b35b932225e30aa060166c4a5bbd
Marco Sulla <github@marco.sulla.e4ward.com>
2019-08-02 15:18:26 +0200

    Update __init__.py
    added `fromkeys`, only for completeness :)

--- frozendict/__init__.py
+++ frozendict/__init__.py
@@ -18,31 +18,35 @@ class frozendict(collections.Mapping):
     An immutable wrapper around dictionaries that implements the complete :py:class:`collections.Mapping`
     interface. It can be used as a drop-in replacement for dictionaries where immutability is desired.
     """
-
+    
     dict_cls = dict
-
+    
+    @classmethod
+    def fromkeys(cls, seq, value=None, *args, **kwargs):
+        return cls(dict.fromkeys(seq, value, *args, **kwargs))
+    
     def __init__(self, *args, **kwargs):
         self._dict = self.dict_cls(*args, **kwargs)
         self._hash = None
-
+    
     def __getitem__(self, key):
         return self._dict[key]
-
+    
     def __contains__(self, key):
         return key in self._dict
-
+    
     def copy(self, **add_or_replace):
         return self.__class__(self, **add_or_replace)
-
+    
     def __iter__(self):
         return iter(self._dict)
-
+    
     def __len__(self):
         return len(self._dict)
-
+    
     def __repr__(self):
         return '<%s %r>' % (self.__class__.__name__, self._dict)
-
+    
     def __hash__(self):
         if self._hash is None:
             h = 0
